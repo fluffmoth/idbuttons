08ec55b19c92c66ebd834d65ecedfa5b9808c174
fluffmoth <cbickle@gmail.com>
2026-08-14 01:17:39 -0400

    support shortened imgbb and imgur links

--- public/buttonFunctions.js
+++ public/buttonFunctions.js
@@ -513,8 +513,14 @@ function getHTMLCode(btn) {
 // if no sources, return blank string
 // optional full parameter = whether to get full location url for local sources
 function getSrc(btn,full) {
-	if (btn.imgbb && btn.imgbb != undefined && btn.imgbb != ``) return btn.imgbb;
-	else if (btn.imgur && btn.imgur != undefined && btn.imgur != ``) return btn.imgur;
+	if (btn.imgbb && btn.imgbb != undefined && btn.imgbb != ``) {
+		if (!btn.imgbb.includes('https:')) btn.imgbb = 'https://i.ibb.co/' + btn.imgbb;
+		return btn.imgbb;
+	}
+	else if (btn.imgur && btn.imgur != undefined && btn.imgur != ``) {
+		if (!btn.imgur.includes('https:')) btn.imgur = 'https://i.imgur.com/' + btn.imgur;
+		return btn.imgur;
+	}
 	else if (btn.postimg && btn.postimg != undefined && btn.postimg != ``) return btn.postimg;
 	else if (btn.src) return getLocalSrc(btn,true);
 	else {
